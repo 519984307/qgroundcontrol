9eefd4736af01e6629757cb074a178ca9caf1e87
Adrian Schneebeli <aschneebeli@jedsy.com>
2022-07-08 08:50:25 +0200

    properly send negative step commands

--- src/AutoPilotPlugins/Common/HookComponentController.cc
+++ src/AutoPilotPlugins/Common/HookComponentController.cc
@@ -39,19 +39,22 @@ HookComponentController::_sendHookStepCommand(int num_steps)
         return;
     }
 
-    int sign;
+    uint16_t sign;
     if(num_steps >= 0) {
-        sign = 1;
+        // first bit == 0 means positive amount of steps
+        sign = 0;
     } else {
-        sign = -1;
+        // first bit == 1 means negative amount of steps
+        sign = 1;
     }
+    // now that have the sign, make sure num_steps is positive for the bit
+    // banging below
+    num_steps = abs(num_steps);
 
     uint16_t steps_to_send;
     steps_to_send |= sign << 15; // first bit is sign
-    // other bits are number of steps. We just mask out the first bit of
-    // num_steps, if someone wants to move more than 32768 steps at once they
-    // have other problems
-    steps_to_send |= (num_steps && 0x7fff);
+    // other bits are number of steps. We just mask out the first bit
+    steps_to_send |= static_cast<uint16_t>(num_steps & 0x7fff);
 
     mavlink_message_t msg;
     mavlink_msg_hook_command_pack_chan(
